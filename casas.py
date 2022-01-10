50685b231cd342539ade92808c8bc784daa006a3
Mateus Cabral <jmateuscabral@gmail.com>
2022-01-10 17:48:29 -0300

    Criando classe básica das casas e dos cavaleiros

---
+++ casas.py
@@ -0,0 +1,26 @@
+"""
+50 Áries
+55 Touro
+60 Gêmeos
+70 Câncer
+75 Leão
+80 Virgem
+85 Libra
+90 Escorpião
+95 Sargitário
+100 Capricórnio
+110 Aquário
+120 Peixes
+"""
+
+"""
+tempo = dificuldade_da_casa / poder_dos_cavaleiros
+"""
+
+
+class Casa:
+
+    def __init__(self, nome, dificuldade):
+
+        self.nome = nome
+        self.dificuldade = dificuldade
---
+++ cavaleiros.py
@@ -0,0 +1,15 @@
+"""
+1.5 Seiya
+1.4 Shiryu
+1.3 Hyoga
+1.2 Shun
+1.1 Ikki
+"""
+
+
+class Cavaleiro:
+
+    def __init__(self, nome, poder_cosmico):
+
+        self.nome = nome
+        self.poder_cosmico = poder_cosmico
